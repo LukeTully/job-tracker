37a8736e9731ab4b770474e31c55578ace3900e9
Luke <martinlm@umanitoba.ca>
2021-03-05 18:32:47 -0800

    Create a service api for interacting with storage layer

---
+++ src/services/index.js
@@ -0,0 +1,45 @@
+import * as localForage from "localforage";
+import {
+  generateIndex
+} from "../utilities";
+
+import
+error
+from "../services/errors";
+
+export async function create(key = null, value) {
+  let index = key;
+  if (key === null) {
+    index = generateIndex(value.title, value.company);
+    value["index"] = index;
+  }
+  console.log(`The last index saved was: ${index} and belong to ${value.title}`);
+  await localForage.setItem(index, value);
+}
+export async function remove(key) {
+  await localForage.removeItem(key);
+}
+export async function update(key, newValue) {
+  try {
+    const item = await localForage.getItem(key);
+    if (item === null) {
+      throw new Error("Item was not in the database");
+    }
+    remove(key);
+    const newKey = generateIndex(newValue.title, newValue.company);
+    newValue['index'] = newKey;
+    await create(newKey, newValue);
+  } catch (err) {
+    error(err);
+  }
+}
+export async function all() {
+  const jobs = [];
+  await localForage.iterate((val, id) => {
+    jobs.push(val)
+  });
+  return jobs;
+}
+export async function read(id) {
+  return localForage.getItem(id);
+}
