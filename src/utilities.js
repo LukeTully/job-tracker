fd809ad3a53913dadc61f0b302f27e092adb8f49
Luke <martinlm@umanitoba.ca>
2021-03-05 18:34:57 -0800

    Pull util methods into separate module

---
+++ src/utilities.js
@@ -0,0 +1,15 @@
+export function generateTimestamp() {
+    return performance.now();
+}
+
+export function generateIndex(title, company) {
+    const initialIndex = `${title}-at-${company}`;
+    const spacesToDashes = initialIndex.replace(/\s+/g, '-');
+    const lowercase = spacesToDashes.toLowerCase();
+    const punctuationRemoved = lowercase.replace(/[^a-zA-Z0-9\s-]+/g, '');
+    return encodeURIComponent(punctuationRemoved);
+}
+
+export function validateMinimumSalary(salary) {
+    // Only check that the value is 
+}
