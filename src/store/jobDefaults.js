95f50edee666b8375ae56f21e0fc3d1d6fec5dc0
Luke <martinlm@umanitoba.ca>
2021-03-05 18:33:20 -0800

    Isolate defaults for job props

---
+++ src/store/jobDefaults.js
@@ -0,0 +1,15 @@
+export default {
+  index: 0,
+  title: "",
+  company: "",
+  salaryMin: 0,
+  salaryMax: 0,
+  description: "",
+  experienceLevel: "Senior",
+  tech: [],
+  applied: false,
+  interviewCount: 0,
+  easyApply: false,
+  requiresPreInterviewTest: false,
+  requiresHomework: false,
+};
