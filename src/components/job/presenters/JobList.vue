e7de3f0195bfeaa2673b9fedb71fa2ea672cbc85
Luke <martinlm@umanitoba.ca>
2021-03-09 13:58:37 -0800

    Move presentaional components to their own folder

---
+++ src/components/job/presenters/JobList.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-aside>
+    <router-link :to="{ name: 'newJob' }">
+      <el-button>New Job</el-button>
+    </router-link>
+    <el-menu id="job-list" router>
+      <router-link :to="{ name: 'summary' }">Overview</router-link>
+      <router-link
+        v-for="job in jobs"
+        :key="job.index"
+        :to="{ name: 'job', params: { id: job.index } }"
+      >
+        <job-list-item v-bind="job"></job-list-item>
+      </router-link>
+    </el-menu>
+  </el-aside>
+</template>
+
+<script>
+import { Aside, MenuItem, Menu, Button } from "element-ui";
+import JobListItem from "../components/JobListItem";
+
+export default {
+  name: "JobList",
+  props: {
+    jobs: Array
+  },
+  components: {
+    [Aside.name]: Aside,
+    [MenuItem.name]: MenuItem,
+    [Menu.name]: Menu,
+    [Button.name]: Button,
+    JobListItem
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.el-menu {
+  height: 100%;
+  margin-top: 2rem;
+  padding: 1rem;
+  border: 0;
+}
+.el-aside {
+  border-right: 1px solid #ddd;
+  padding-top: 1rem;
+}
+</style>
---
+++ src/components/job/presenters/JobListItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-menu-item>
+    {{ title }}
+  </el-menu-item>
+</template>
+
+<script>
+import { MenuItem } from "element-ui";
+import { jobProps } from "../../../constants";
+
+export default {
+  name: "JobListItem",
+  props: {
+    ...jobProps
+  },
+  components: {
+    [MenuItem.name]: MenuItem
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.el-menu-item {
+  max-width: 100%;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  text-align: left;
+}
+</style>
