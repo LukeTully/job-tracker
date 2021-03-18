5b1b1cfe39dd292734f0d08b6101f9f8eb8117e9
Luke <martinlm@umanitoba.ca>
2021-03-18 05:15:22 -0700

    Switches exampleData static file from json to js

---
+++ src/store/exampleData.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    title: 'Senior JS Developer',
+    company: 'Webrenos inc.',
+    salaryMin: 100000,
+    salaryMax: 130000,
+    description:
+      'Sage Recruiting is looking\r\n  for a Senior Frontend JavaScript Software Engineer to join the team of a dynamic company based out of beautiful Vancouver,\r\n  BC!You will be working with a small team of innovators who are working towards cracking the code in highly regulated industries such as banking & insurance.This team is not satisfied with the status quo in most regulatory industries and is working on changing things.Security is their holy grail,\r\n  and privacy is their corner store!\r\n\r\n\r\n  We \'re looking for someone that has solid JavaScript and TypeScript coding experience using modern frameworks like React and Redux. Your primary focus will be on developing user-facing components and implementing them following well-known React.js workflows. You will ensure that these components and the overall application are robust and are easy to maintain. This is an incredible opportunity to scale up your career with an extremely driven team located in Gastown, Vancouver!\r\n\r\n\r\n\r\n  What we are looking for:\r\n\r\n    At least 5 years of JavaScript software development experience\r\n  Experience working in an agile development environment\r\n  A concrete understanding of React.js\r\n  Expertise with TypeScript\r\n  Experience with WebPack\r\n  Code samples or a GitHub portfolio is preferred\r\n  Experience with node.js is nice to have\r\n  Experience building reusable components and front - end libraries\r\n  for future use\r\n  Ability to translate designs and wireframes into high - quality code\r\n  Experience with backend programming or DevOps is nice to have(but we are okay with 100 % frontend folks as well!)\r\n\r\n\r\n\r\n  What we offer:\r\n\r\n    Competitive salary,\r\n  vacation,\r\n  and benefits\r\n  Equity\r\n  Remote work(but we also have a renovated two - story office in Gastown Vancouver with a rooftop patio)\r\n  Ability to be challenged and work with some of the best in the industry!\r\n\r\n\r\n\r\n  Please apply with your resume and GitHub profile or code samples.GitHub is not required but will get you to the top of the list!\r\n\r\n\r\n    Salary is commensurate with experience.Only qualified candidates will be contacted.Thank you to all of the applicants!',
+    experienceLevel: 'Senior',
+    tech: [
+      'TypeScript',
+      'JavaScript',
+      'Webpack',
+      'React.js',
+      'Node.js',
+      'Redux',
+    ],
+    applied: false,
+    interviewCount: 0,
+    easyApply: false,
+    requiresPreInterviewTest: false,
+    requiresHomework: false,
+  },
+]
